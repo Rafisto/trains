9ddcb60fcd958c1f1a31cbeccdef1603b7f371b5
Rafisto <1.rafal.wlodarczyk@gmail.com>
2023-10-01 20:58:16 +0200

    Base API idea.

---
+++ scrapKoleo.py
@@ -0,0 +1,21 @@
+import requests as req
+
+start_range = 999
+end_range = 2000
+entries = []
+
+for iter in range(start_range,end_range):
+    request = req.get(f"https://koleo.pl/ls?q={iter}")
+    if request.status_code == 200:
+        res = request.json()
+        try:
+            station_name = res['stations'][0]['name']
+            ibnr = res['stations'][0]['ibnr']
+            entries.append(f"- {ibnr}: {station_name}")
+            print(f"Found {ibnr}: {station_name}")
+        except IndexError:
+            print(f"Index error raised on index {iter}")
+            print(f"Details: {res}")
+
+with open("stations.to.1k.txt", "w", encoding="utf-8") as file:
+    file.write('\n'.join(entries))
